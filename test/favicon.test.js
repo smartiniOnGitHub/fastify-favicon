a37700487e44c86d82115fa84aee97567dfdd92c
Sandro Martini <sandro.martini@gmail.com>
2018-02-12 19:20:42 +0100

    update test (now in a dedicated folder and no more in the same folder of the plugin)

--- test/favicon.test.js
+++ test/favicon.test.js
@@ -5,9 +5,9 @@ const request = require('request')
 const Fastify = require('fastify')
 
 test('default favicon does not return an error, but a good response (200) and some content', (t) => {
-  t.plan(2)
+  t.plan(4)
   const fastify = Fastify()
-  fastify.register(require('./'))  // configure this plugin with its default options
+  fastify.register(require('../'))  // configure this plugin with its default options
 
   fastify.listen(0, (err) => {
     fastify.server.unref()
@@ -21,13 +21,13 @@ test('default favicon does not return an error, but a good response (200) and so
       t.error(err)
       t.strictEqual(response.statusCode, 200)
       t.strictEqual(response.headers['content-type'], 'image/x-icon')
-//      t.strictEqual(response.headers['content-length'], '' + body.length)
+      // TODO: add check on file conmtents, or at least file size ...
 
       fastify.close()
     })
   })
 })
 
-// TODO: add test to return an icon ...
+// TODO: add test to check error code when returning a not-existing favicon ...
 
-// TODO: add a test to return a favicon configured in a custom path ...
+// TODO: add a test to return a favicon configured in a custom path (but provided here as example/test resources) ...
