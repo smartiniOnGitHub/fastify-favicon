ce4a242d55e48411d8d3c15f678bde6e91ac86e8
Sandro Martini <sandro.martini@gmail.com>
2018-02-17 10:44:52 +0100

    add Fastify favicon and related attributions and license; update my sources adding license header; add a sample static home page in example and use it in example minimal server (optional)

--- example/example.js
+++ example/example.js
@@ -1,8 +1,32 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict'
 
 const fastify = require('fastify')()
 
 fastify.register(require('../plugin'), {
+})
+
+// example to handle a sample home request to serve a static page, optional here
+fastify.get('/', function (req, reply) {
+  const path = require('path')
+  const scriptRelativeFolder = __dirname + path.sep
+  const fs = require('fs')
+  const stream = fs.createReadStream(scriptRelativeFolder + 'home.html')
+  reply.type('text/html').send(stream)
 })
 
 fastify.listen(3000, '0.0.0.0', (err) => {
